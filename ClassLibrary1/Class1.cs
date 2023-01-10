ec9189cbfeb591017e1e47bcd26aa4c7dde0600f
RISHIK SHRIVASTAV <rishikshrivastav0@gmail.com>
2023-01-10 19:00:25 +0530

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*using System.Windows.Forms;*/
+namespace ClassLibrary1
+
+{
+    public class ClassLibrary
+    {
+        public void message()
+        {
+            /*MessageBox.Show();*/
+            Console.WriteLine("I am great");
+            Console.Read();
+        }
+    }
+}
